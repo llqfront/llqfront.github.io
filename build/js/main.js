7db11e4797ff08f95185765d87d327d8dbe75d75
fe <liuliqing@jiyoutang.com>
2015-12-12 22:43:12 +0800

    up

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,2 @@
-/* main.js Date:2015-12-12 22:40:10 */
+/* main.js Date:2015-12-12 22:42:49 */
 console.log("This would be the main JS file.");
--- build/js/unparam.js
+++ build/js/unparam.js
@@ -1,2 +1,2 @@
-/* unparam.js Date:2015-12-12 22:40:10 */
+/* unparam.js Date:2015-12-12 22:42:48 */
 !function(a){a.unparam=function(b){var c,d,e=/\[(.*?)\]/g,f=/(.+?)\[/,g={};return"string"!=(d=a.type(b))||"string"==d&&!d.length?{}:(c=decodeURIComponent(b).split("&"),!(d=c.length)||1==d&&""===d?g:(a.each(c,function(b,c){if(c.length){d=c.split("=");var j,k,h=d.shift(),i=d.join("=").replace(/\+/g," "),l=[];if(h.length){for(;d=e.exec(h);)l.push(d[1]);if(!(j=l.length))return void(g[h]=i);j--,d=f.exec(h),d&&(h=d[1])&&h.length&&("object"!=a.type(g[h])&&(g[h]={}),k=g[h],a.each(l,function(b,c){(d=c).length||(d=0,a.each(k,function(a){!isNaN(a)&&a>=0&&a%1===0&&a>=d&&(d=Number(a)+1)})),b==j?k[d]=i:k="object"!=a.type(k[d])?k[d]={}:k[d]}))}}}),g))}}(jQuery);
